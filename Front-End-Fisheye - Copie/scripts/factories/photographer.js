230bd6048b982505137743e387dc205bf053a403
Adeline Gorius <adeline.gorius@gmail.com>
2023-03-09 09:52:42 +0100

    Add files via upload

---
+++ Front-End-Fisheye - Copie/scripts/factories/photographer.js
@@ -0,0 +1,40 @@
+function photographerFactory(data) {
+  const { id, name, portrait, city, tagline, price } = data;
+
+  const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
+  const lienPhotographe = `http://127.0.0.1:5500/photographer.html?id=${id}`;
+
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.classList.add("portrait");
+    const h2 = document.createElement("h2");
+
+    h2.textContent = name;
+    h2.classList.add("name");
+
+    const a = document.createElement("a");
+    a.setAttribute("href", lienPhotographe);
+
+    const h3 = document.createElement("h3");
+    h3.textContent = city;
+    h3.classList.add("location");
+    const tag = document.createElement("p");
+    tag.classList.add("tagline");
+    tag.textContent = tagline;
+    const prix = document.createElement("p");
+    prix.classList.add("price");
+    prix.textContent = price + "€ /jour";
+    article.appendChild(img);
+    a.appendChild(img);
+    a.appendChild(h2);
+    article.appendChild(a);
+    article.appendChild(h3);
+    article.appendChild(tag);
+    article.appendChild(prix);
+
+    return article;
+  }
+  return { id, name, picture, city, tagline, price, getUserCardDOM };
+}
---
+++ Front-End-Fisheye - Copie/scripts/pages/index.js
@@ -0,0 +1,38 @@
+//let photographers = [];
+
+const fetchphotographers = async () => {
+  await fetch("data/photographers.json")
+    .then((response) => response.json())
+    .then((promise) => {
+      photographers = promise;
+      // console.log(photographers);
+    });
+};
+async function getPhotographers() {
+  // et bien retourner le tableau photographers seulement une fois récupéré
+  await fetchphotographers();
+
+  return {
+    photographers,
+  };
+}
+
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    console.log(photographerModel);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+
+  displayData(photographers);
+}
+
+init();
