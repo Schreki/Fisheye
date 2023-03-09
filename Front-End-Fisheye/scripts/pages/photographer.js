799d9511e43eed63d59f62264bb9c8f136f4d8ca
Adeline Gorius <adeline.goriusgmail.com>
2023-03-09 09:35:58 +0100

    Envoi du projet

--- Front-End-Fisheye/scripts/pages/photographer.js
+++ Front-End-Fisheye/scripts/pages/photographer.js
@@ -1,1 +1,56 @@
 //Mettre le code JavaScript lié à la page photographer.html
+const url = new URL(document.location);
+const searchParams = url.searchParams;
+idPhotographe = searchParams.get("id");
+console.log(idPhotographe);
+
+/*
+function photographerPageFactory(data) {
+  const { idPhotographe, name, portrait, city, tagline, price } = data;
+
+  const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
+
+  function getPhotographerHeaderDOM() {
+    const article = document.createElement("article");
+    console.log(article);
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.classList.add("portrait");
+    const h2 = document.createElement("h2");
+
+    h2.textContent = name;
+    h2.classList.add("name");
+
+    const a = document.createElement("a");
+    a.setAttribute("href", lienPhotographe);
+
+    const h3 = document.createElement("h3");
+    h3.textContent = city;
+    h3.classList.add("location");
+    const tag = document.createElement("p");
+    tag.classList.add("tagline");
+    tag.textContent = tagline;
+    const prix = document.createElement("p");
+    prix.classList.add("price");
+    prix.textContent = price + "€ /jour";
+    article.appendChild(img);
+    a.appendChild(img);
+    a.appendChild(h2);
+    article.appendChild(a);
+    article.appendChild(h3);
+    article.appendChild(tag);
+    article.appendChild(prix);
+
+    return article;
+  }
+  return {
+    idPhotographe,
+    name,
+    picture,
+    city,
+    tagline,
+    price,
+    getPhotographerHeaderDOM,
+  };
+}
+*/
