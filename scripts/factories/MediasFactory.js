436cc047ce7234a57f4cb92c2aeeb88a237f6f0f
Adeline Gorius <adeline.goriusgmail.com>
2023-03-29 13:04:23 +0200

    contenu statique page photographe

--- scripts/factories/MediasFactory.js
+++ scripts/factories/MediasFactory.js
@@ -1,27 +1,50 @@
-function mediaFactory(media){
+function mediaFactory(media) {
+  if (media.image !== undefined) {
+    return createImage(media.image, media.title, media.likes);
+  }
 
-    if(media.image !== undefined){
-
-        return createImage(media.image, media.title);
-
-    }
-
-    return createVideo(media.video, media.title);
-
+  return createVideo(media.video, media.title, media.likes);
 }
 
-function createImage(imgSrc, imgAlt){
-
-     return `<img src="assets/medias/${imgSrc}" alt="${imgAlt}"  />`;
-
+function createImage(imgSrc, imgAlt, likes) {
+  return `
+  <article class="photograph-media-item">
+  
+     <img src="assets/medias/${imgSrc}" alt="${imgAlt}"  />
+        <section class="media-card-info"> 
+            <h2 class="media-card-title">${imgAlt}</h2>
+        
+        <div class="media-like-container">
+          <span class="media-like-count">${likes}</span>
+          <button class="media-like-button" aria-label="Bouton de likes">
+            <i class="media-like-logo fa-heart fa-regular"></i>
+          </button></section>
+        </div>
+     </article>
+     
+   `;
 }
 
-function createVideo(videoSrc, videoAlt){
-
-    return `<video>
-              <source src="assets/medias/${videoSrc}" type="video/mp4" alt="${videoAlt}"></source>
+function createVideo(videoSrc, videoAlt, videoLikes) {
+  return `
+    <article class="photograph-media-item">
+        
+            <video>
+                <source src="assets/medias/${videoSrc}" type="video/mp4" alt="${videoAlt}"></source>
             </video>
+            <section class="media-card-info"> 
+            <h2 class="media-card-title">${videoAlt}</h2>
+
+            <div class="media-like-container">
+          <span class="media-like-count">${videoLikes}</span>
+          <button class="media-like-button" aria-label="Bouton de likes">
+            <i class="media-like-logo fa-heart fa-regular"></i>
+          </button>
+          
+        </div>
+        </section>
+        
+    </article>
     
             `;
-
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -13,14 +13,14 @@ fetchData("data/photographers.json").then((photographersInfos) => {
     idPhotographe,
     photographersInfos.media
   );
-
   displayPhotographerInfos(photographerInfos);
-
   displayPhotograherMedias(photographerMedias);
+  displayPhotographerLike();
+  displayPhotographerPrice(photographerInfos.price);
+  addEventListeners();
 });
 
 // Fonction qui recupere les informations d'un photographe
-
 function getPhotographerInfos(photographerId, arrayOfPhotographers) {
   return arrayOfPhotographers.find((photographerInfos) => {
     return Number(photographerId) === Number(photographerInfos.id);
@@ -28,55 +28,101 @@ function getPhotographerInfos(photographerId, arrayOfPhotographers) {
 }
 
 // Fonction qui recupere les medias d'un photographe
-
 function getPhotographerMedias(photographerId, arrayOfPhotographersMedias) {
   return arrayOfPhotographersMedias.filter((photographerMedias) => {
     return Number(photographerId) === Number(photographerMedias.photographerId);
   });
 }
 
-// Cette fonction affiche les informations d'un photographe
-
+// Fonction qui affiche les informations d'un photographe
 function displayPhotographerInfos(photographerInfos) {
   const html = `
      
           <article class="photographer-header-item">
-               <div class="photographer-profile">
-                    <h1>${photographerInfos.name}</h1>
-                    <h3>${photographerInfos.city}, ${photographerInfos.country}</h3>
-                    <p>${photographerInfos.tagline}</p>
-               </div>
-          
+            <div class="photographer-profile">
+              <h1>${photographerInfos.name}</h1>
+              <h3>${photographerInfos.city}, ${photographerInfos.country}</h3>
+                <p>${photographerInfos.tagline}</p>
+            </div>
           </article>
-
-          <article class="photographer-header-item">
-          
-          <button class="contact_button photographer-header-item" onclick="displayModal()">Contactez-moi</button>
-          
+          <article class="photographer-header-item">          
+            <button class="contact_button photographer-header-item" onclick="displayModal()">Contactez-moi</button>
           </article>
-
           <article class="photographer-header-item">
-          
               <img src="assets/photographers/Photographers ID Photos/${photographerInfos.portrait}" alt="${photographerInfos.name}" />
-
           </article>
-     
-     `;
+       `;
 
   document.querySelector(".photograph-header").innerHTML = html;
 }
 
 function displayPhotograherMedias(arrayOfMedias) {
+  let mediasHTML = "";
 
-     let mediasHTML = "";
-    
-     arrayOfMedias.forEach((media)=>{
-
-          mediasHTML += `
-          
-               ${mediaFactory(media)}
+  arrayOfMedias.forEach((media) => {
+    mediasHTML += `          
+            ${mediaFactory(media)}
           `;
-     });
+  });
 
-     document.querySelector(".photograph-media").innerHTML =  mediasHTML;
+  document.querySelector(".photograph-media").innerHTML = mediasHTML;
+}
+//Fonction qui affiche le nombre de like total d'un photographe
+function displayPhotographerLike() {
+  const mediaLikeCount = document.querySelectorAll(".media-like-count");
+  let totalMediaLikeCount = 0;
+
+  mediaLikeCount.forEach((media) => {
+    totalMediaLikeCount += Number(media.textContent);
+  });
+  let likeHtml = `
+    
+      <div class="footer-container">
+        <span class="footer-likes" id="totalLikesCount">${totalMediaLikeCount}</span>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+      <p class="price"></p>
+
+    `;
+  document.querySelector(".footer").innerHTML = likeHtml;
+}
+//Fonction qui affiche le prix d'un photographe
+function displayPhotographerPrice(price) {
+  const mediaLikeCount = document.querySelectorAll(".media-like-count");
+  let totalMediaLikeCount = 0;
+  mediaLikeCount.forEach((media) => {
+    totalMediaLikeCount += Number(media.textContent);
+  });
+  let priceHtml = `  
+    ${price} €/ jour
+    `;
+  document.querySelector(".price").innerHTML = priceHtml;
+}
+
+// Fonction qui réagit au clique du like
+function addEventListeners() {
+  const mediaCardLikeButtons = document.querySelectorAll(".media-like-button");
+  mediaCardLikeButtons.forEach((button) => {
+    button.addEventListener("click", renderLikes);
+  });
+}
+
+// Fonction qui modifie le nombre de like
+function renderLikes() {
+  const mediaLikeSpanEl = this.parentNode.firstElementChild;
+  const mediaLikeIconEl = this.firstElementChild;
+
+  if (mediaLikeIconEl.classList.contains("fa-regular")) {
+    let mediaLikeCount = Number(mediaLikeSpanEl.textContent);
+    mediaLikeCount++;
+    mediaLikeSpanEl.textContent = mediaLikeCount;
+    displayPhotographerLike();
+    mediaLikeIconEl.classList.replace("fa-regular", "fa-solid");
+  } else if (mediaLikeIconEl.classList.contains("fa-solid")) {
+    let mediaLikeCount = Number(mediaLikeSpanEl.textContent);
+    mediaLikeCount--;
+    mediaLikeSpanEl.textContent = mediaLikeCount;
+    displayPhotographerLike();
+    mediaLikeIconEl.classList.replace("fa-solid", "fa-regular");
+  }
 }
